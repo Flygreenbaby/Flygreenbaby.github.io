af07292a94132c27f78a4ea448882c42ae94b78a
Flygreenbaby <hxqlst@163.com>
2024-09-24 17:40:20 +0800

    Site updated: 2024-09-24 17:40:19

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2022/08/19/Five/","2022/07/27/Four/","2022/07/21/Third/","2022/07/17/Second/","2022/06/30/First/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
