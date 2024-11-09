13b1078af5b90a3ee6ee1104d81e19faf615578c
Flygreenbaby <hxqlst@163.com>
2024-11-09 21:08:58 +0800

    Site updated: 2024-11-09 21:08:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/08/19/Five/","2022/07/27/Four/","2022/07/21/Third/","2022/07/17/Second/","2022/06/30/First/"];function toRandomPost(){
+var posts=["2024/11/09/Six/","2022/08/19/Five/","2022/07/27/Four/","2022/07/21/Third/","2022/07/17/Second/","2022/06/30/First/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
