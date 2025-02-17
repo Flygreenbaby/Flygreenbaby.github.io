62b7d3c3ed80712ef812d3f48e1d83156fd7c35b
Flygreenbaby <hxqlst@163.com>
2025-02-17 13:47:22 +0800

    二维码设计成功

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+// 等待页面加载完成后生成二维码
+window.onload = function() {
+    try {
+        const qrcodeElement = document.getElementById('qrcode');
+        if (!qrcodeElement) {
+            console.error('找不到 ID 为 qrcode 的元素');
+            return;
+        }
+
+        const currentURL = window.location.href;
+        const qrcode = new QRCode(qrcodeElement, {
+            text: currentURL,
+            width: 128,
+            height: 128,
+            colorDark: '#000000',
+            colorLight: '#ffffff',
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    } catch (error) {
+        console.error('生成二维码时出错：', error);
+    }
+}
